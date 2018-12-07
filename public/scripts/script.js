a38790fd7350f50de2b793ffadc6ec77a3cabbe2
Hudson Lessa <admin@hudsonlessa.com>
2018-12-07 20:30:15 -0200

    Add collapsible menu

---
+++ public/scripts/script.js
@@ -0,0 +1,7 @@
+const collapsibleMenuButton = document.querySelector("#collapsible-menu-button");
+const collapsibleMenu = document.querySelector("#collapsible-menu");
+
+collapsibleMenuButton.addEventListener("click" , function()
+{
+    collapsibleMenu.classList.toggle("menu-active");
+});
